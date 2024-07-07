d8a6aa4c7340f73cfb78c0098b3ea9cf0fc240f4
Lee Damil <lidamil6785@gmail.com>
2024-07-07 23:20:52 +0600

    Commit

---
+++ NoGames.py
@@ -0,0 +1,71 @@
+import webbrowser, psutil
+
+prog_list = []
+
+file = open("Exes.txt", "r")
+
+for line in file:
+    prog_list.append(line.strip())
+
+exe = False
+
+def open_browser():
+    webbrowser.open_new("https://i.pinimg.com/736x/5c/b7/37/5cb737b195f6cc3c3e3a0e53e5aaab99.jpg")
+
+def kill_process():
+    global exe
+
+    for process in (process for process in psutil.process_iter() if process.name() in prog_list):
+        exe = True
+        process.kill()
+
+    if exe:
+        open_browser()
+    
+    exe = False
+
+while True:
+
+    kill_process()
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
